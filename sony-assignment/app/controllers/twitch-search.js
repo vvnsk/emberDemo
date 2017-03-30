fadba04bbe5d04888bfff021c78b9706ece72a69
Vinnakota Saikrishna <vvnsk92@gmail.com>
2017-03-30 15:52:51 -0400

    Pagination
    Integrated Twitch, Bootstrap and Pagination

--- sony-assignment/app/controllers/twitch-search.js
+++ sony-assignment/app/controllers/twitch-search.js
@@ -2,16 +2,53 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        toggleBody() {
-            this.toggleProperty('isExpanded');
-        },
-        fetchResults(){
-            console.log(this.model.q);
+        fetchResults(origin){
             var that = this;
-            Ember.$.ajax('http://httpbin.org/ip', {
+            var setPrevPage = function(){
+                var page = that.model.page;
+                var totalPages = that.model.totalPages;
+                Ember.run(function() {
+                    if(page > 1 && totalPages > 1) {
+                        that.set('model.prevPage',page - 1);
+                    } else {
+                        that.set('model.prevPage',null);
+                    }
+                    
+                });
+            };
+            var setNextPage = function(){
+                
+                var page = that.model.page;
+                var totalPages = that.model.totalPages;
+                Ember.run(function() {
+                    if(page < totalPages && totalPages > 1) {
+                        that.set('model.nextPage',page + 1);
+                    } else {
+                        that.set('model.nextPage',null);
+                    }
+                });
+            };
+           var setPages = function(){
+                var page = that.model.page;
+                var totalPages = that.model.totalPages;
+                var list = [];
+
+                for(var i=page-2;list.length<5&&i<totalPages;i++){
+                    if(i>0)
+                        list.push(i)
+                }
+                console.log(list);
+                Ember.run(function() {
+                    that.set('model.pages',list);
+                });
+            }
+            Ember.$.ajax('https://api.twitch.tv/kraken/search/streams?query='+that.model.q+'&limit='+that.model.limit+'&offset='+((that.model.page)*that.model.limit), {
                 "type": 'GET', // HTTP method
                 "dataType": 'JSON', // type of data expected from the API response,
                 "async": true,
+                "headers": {
+                    'Client-ID': '8dr67w9awdrzf2kyglknzmhk4wfiii'
+                },
                 "data": { // Begin data payload
                     "auth": {
                         "type" : "basic",
@@ -29,14 +66,27 @@ export default Ember.Controller.extend({
                 }, // End data payload
                 "success": function (data, textStatus, jqXHR) {
                     Ember.run(function() {
-                        that.set('model.searchResults',data);
+                        that.set('model.searchResults',data.streams);
+                        that.set('model.start',false);
+                        if(origin=="search"){
+                            that.set('model.page',0);
+                        }
+                        that.set('model.totalPages',data._total/that.model.limit);
+                        setPages();setPrevPage();setNextPage();
                     });
+                    
                     return data;
                 },
                 "error": function (jqXHR, textStatus, errorThrown) {
                     window.console.log(jqXHR);
                 }
             });
+        },selectPage: function(number) {
+            var that = this;
+            Ember.run(function() {
+                that.set('model.page', number);
+            });
+            this.send('fetchResults','page');
         }
     }
 });
--- sony-assignment/app/routes/twitch-search.js
+++ sony-assignment/app/routes/twitch-search.js
@@ -3,10 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(){
         return{
-            list : ['Injustice', 'Fifa', 'Call of Duty'],
-            q : "hey",
-            isExpanded : true,
-            searchResults : {"origin":'null'}
+            q : "",
+            searchResults : [],
+            start : true,
+            limit : 5,
+            page : 0,
+            totalPages : 0,
+            prevPage : null,
+            nextPage : null,
         }
     }
 });
